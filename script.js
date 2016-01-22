f8462bc22ba11c27ce11dd06158e33d365be791d
Eduard20 <esimonyan@rambler.ru>
2016-01-22 18:46:05 +0400

    Added authorization

--- script.js
+++ script.js
@@ -5,8 +5,26 @@
 jQuery(function($){
     var socket = io.connect();
     var $chat = $("#chat");
+    var $setNick = $("#setNick");
+    var $nickError = $("#nickError");
+    var $nickname = $("#nickname");
     var $messageBox = $('#messageBox');
     var $message = $('#message');
+
+
+    $setNick.submit(function(e){
+        e.preventDefault();
+        socket.emit('new user', $nickname.val(), function(data){
+            if(data) {
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+            } else {
+                $nickError.html('try again baby');
+            }
+        });
+        $nickname.val("");
+    });
+
 
     $messageBox.submit(function(e){
         e.preventDefault();
--- server.js
+++ server.js
@@ -6,12 +6,23 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var nicknames = [];
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html");
 })
 app.use(express.static(__dirname));
 io.sockets.on('connection', function(socket){
+    socket.on('new user', function(data, callback){
+        if(nicknames.indexOf(data) != -1) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            nicknames.push(socket.nickname);
+            io.sockets.emit('usernames', nicknames);
+        }
+    });
     socket.on('send message', function(data){
         io.sockets.emit('new message', data);
         //socket.broadcast.emit('new message', data);
